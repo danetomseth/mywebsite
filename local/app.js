575d0f097ed193ffea6476dea92f287cd3dac05a
danetomseth <danetomseth@gmail.com>
2016-03-24 14:35:56 -0400

    made index and layout templates

--- local/app.js
+++ local/app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig');
+var morgan = require('morgan');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -12,11 +14,20 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
+
+//app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+
+// app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
+// app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+
+app.use(morgan('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,6 +35,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -55,6 +69,8 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+app.listen(3000);
 
 
 module.exports = app;
